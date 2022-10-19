89a1193a2213e4b26226542c7d347a8f553df086
Oksana <oksana.palamarchuk@burda-forward>
2022-10-19 14:36:06 +0200

    Used accordion component with slots

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,12 +2,36 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <accordion-element header="Degree" />
+
+      <accordion-element header="Job types">
+        <p>Placeholder</p>
+      </accordion-element>
+
+      <job-filters-sidebar-organisations />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import AccordionElement from "@/components/Shared/AccordionElement.vue";
+import JobFiltersSidebarOrganisations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganisations.vue";
+
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+    AccordionElement,
+    JobFiltersSidebarOrganisations,
+  },
 };
 </script>
