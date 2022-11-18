91113c6dd13e14de984ced8b3369a39702be7acb
Oksana <oksana.palamarchuk@burda-forward>
2022-11-18 09:48:56 +0100

    Added filter by organisations, used mapGetters

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -34,9 +34,9 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
-import { FETCH_JOBS } from "@/store";
+import { FETCH_JOBS, FILTERED_JOBS_BY_ORGANIZATIONS } from "@/store/constants";
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 export default {
@@ -45,6 +45,7 @@ export default {
     JobListing,
   },
   computed: {
+    ...mapGetters([FILTERED_JOBS_BY_ORGANIZATIONS]),
     currentPage() {
       const pageString = this.$route.query.page || "1";
       return Number.parseInt(pageString);
@@ -56,16 +57,21 @@ export default {
     },
     nextPage() {
       const nextPage = this.currentPage + 1;
-      const maxPage = Math.ceil(this.jobs.length / 10);
+      const maxPage = Math.ceil(
+        this.FILTERED_JOBS_BY_ORGANIZATIONS.length / 10
+      );
       return nextPage <= maxPage ? nextPage : undefined;
     },
     displayedJobs() {
       const pageNumber = this.currentPage;
       const firstJobIndex = (pageNumber - 1) * 10;
       const lastJobIndex = pageNumber * 10;
-      return this.jobs.slice(firstJobIndex, lastJobIndex);
+      return this.FILTERED_JOBS_BY_ORGANIZATIONS.slice(
+        firstJobIndex,
+        lastJobIndex
+      );
     },
-    ...mapState(["jobs"]),
+    // ...mapState(["jobs"]),  we used it to get the original array of jobs from the store before mapGetters (introducing filters)
   },
   async mounted() {
     // this.$store.dispatch(FETCH_JOBS);
