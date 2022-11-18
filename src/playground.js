4847552390111458c35cff090be5b9a96ebbb90a
Oksana <oksana.palamarchuk@burda-forward>
2022-11-18 09:47:05 +0100

    Added JS Sets example

--- src/playground.js
+++ src/playground.js
@@ -6,3 +6,20 @@ const goodFoods = {
 };
 
 console.log(goodFoods);
+
+/* JS Sets (ES6) */
+// Arrays => order
+// Objects => association
+// Set => uniqueness
+
+const numbers = new Set();
+numbers.add(5);
+numbers.add(10);
+numbers.add(15);
+
+numbers.add(5);
+numbers.add(15);
+
+numbers.add(25);
+
+console.log(numbers);
