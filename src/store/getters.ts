8033ab59ed6b9c7210c6833f64af175227a8244e
Oksana <oksana.palamarchuk@burda-forward>
2023-01-09 13:44:17 +0100

    Changed to typescript from javascript

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -8,9 +8,17 @@ import {
   INCLUDE_JOB_BY_ORGANIZATION,
 } from "@/store/constants";
 
+import { GlobalState } from "@/store/types";
+import { Job } from "@/api/types";
+
+interface IncludeJobGetters {
+  INCLUDE_JOB_BY_ORGANIZATION: (job: Job) => boolean;
+  INCLUDE_JOB_BY_JOB_TYPE: (job: Job) => boolean;
+}
+
 const getters = {
-  [UNIQUE_ORGANIZATIONS](state) {
-    const uniqueOrgs = new Set();
+  [UNIQUE_ORGANIZATIONS](state: GlobalState) {
+    const uniqueOrgs = new Set<string>();
     state.jobs.forEach((job) => uniqueOrgs.add(job.organization));
     return uniqueOrgs;
   },
@@ -23,8 +31,8 @@ const getters = {
   //     state.selectedOrgs.includes(job.organization)
   //   );
   // },
-  [UNIQUE_JOB_TYPES](state) {
-    const uniqueJobTypes = new Set();
+  [UNIQUE_JOB_TYPES](state: GlobalState) {
+    const uniqueJobTypes = new Set<string>();
     state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
     return uniqueJobTypes;
   },
@@ -37,15 +45,15 @@ const getters = {
   //     state.selectedJobTypes.includes(job.jobType)
   //   );
   // },
-  [INCLUDE_JOB_BY_ORGANIZATION]: (state) => (job) => {
+  [INCLUDE_JOB_BY_ORGANIZATION]: (state: GlobalState) => (job: Job) => {
     if (state.selectedOrgs.length === 0) return true;
     return state.selectedOrgs.includes(job.organization);
   },
-  [INCLUDE_JOB_BY_JOB_TYPE]: (state) => (job) => {
+  [INCLUDE_JOB_BY_JOB_TYPE]: (state: GlobalState) => (job: Job) => {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
-  [FILTERED_JOBS](state, getters) {
+  [FILTERED_JOBS](state: GlobalState, getters: IncludeJobGetters) {
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_BY_ORGANIZATION(job))
       .filter((job) => getters.INCLUDE_JOB_BY_JOB_TYPE(job));
---
+++ tests/unit/store/mutations.test.ts
@@ -0,0 +1,44 @@
+import mutations from "@/store/mutations";
+import { createJob, createState } from "./utils";
+
+describe("mutations", () => {
+  describe("LOGIN_USER", () => {
+    it("logs the user in", () => {
+      const startingState = createState({ isLoggedIn: false });
+      mutations.LOGIN_USER(startingState);
+      expect(startingState.isLoggedIn).toBe(true);
+    });
+  });
+
+  describe("RECEIVE_JOBS", () => {
+    it("receives jobs from API response", () => {
+      const startingState = createState({ jobs: [] });
+      const jobOne = createJob();
+      const jobTwo = createJob();
+      mutations.RECEIVE_JOBS(startingState, [jobOne, jobTwo]);
+      expect(startingState.jobs).toEqual([jobOne, jobTwo]);
+    });
+  });
+
+  describe("ADD_SELECTED_ORGS", () => {
+    it("updates organizations that the user has chosen to filter jobs by", () => {
+      const startingState = createState({ selectedOrgs: [] });
+      mutations.ADD_SELECTED_ORGS(startingState, ["Org1", "Org2"]);
+      expect(startingState.selectedOrgs).toEqual(["Org1", "Org2"]);
+    });
+  });
+
+  describe("ADD_SELECTED_JOB_TYPES", () => {
+    it("updates job types that the user has chosen to filter jobs by", () => {
+      const startingState = createState({ selectedJobTypes: [] });
+      mutations.ADD_SELECTED_JOB_TYPES(startingState, [
+        "Full-time",
+        "Part-time",
+      ]);
+      expect(startingState.selectedJobTypes).toEqual([
+        "Full-time",
+        "Part-time",
+      ]);
+    });
+  });
+});
