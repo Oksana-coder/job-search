d82e73710355f57ff2335e240d806956bbe6f608
Oksana <oksana.palamarchuk@burda-forward>
2023-01-12 11:44:51 +0100

    Changed to composition api and typescript

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -2,14 +2,27 @@
   <div>Job Page for job {{ currentJobId }}</div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { useRoute } from "vue-router";
+
+export default defineComponent({
   name: "JobView",
-  computed: {
-    currentJobId() {
-      console.log(this.$route);
-      return this.$route.params.id;
-    },
+  // COMPOSITION API:
+  setup() {
+    const route = useRoute();
+    const currentJobId = route.params.id;
+
+    return { currentJobId };
   },
-};
+
+  // OPTIONS API:
+  // computed: {
+  //   currentJobId() {
+  //     console.log(this.$route);
+  //     return this.$route.params.id;
+  //   },
+  // },
+});
 </script>
